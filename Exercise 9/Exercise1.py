ccf6601b9445972f2a2db4bdef055c52e4a25113
Pedro De Miranda Arthur <pedrohma95@gmail.com>
2017-12-14 21:31:33 -0800

    Fixing Exercise 9

--- Exercise 9/Exercise1.py
+++ Exercise 9/Exercise1.py
@@ -5,20 +5,26 @@ number = randint(0,9)
 print("Let's play guessing game!")
 
 playAgain = True
+trying = 0
 
 while(playAgain):
-    userNumber = int(input("Enter a number from 0 to 9"))
+    userNumber = int(input("Enter a number from 0 to 9\n"))
 
     if(number == userNumber):
-        print("You won!")
-        playAgain == False
+        print("You won! You tryed " + str(trying) + " times!")
+        resp = input("Do you wanna play again? (Y/exit)\n")
+        if(resp == "exit"):
+            playAgain == False
+        else:
+            number = randint(0,9)
+            trying = 0
     elif (userNumber > number):
         print("Your number was grater than the number...")
+        trying += 1
     elif(userNumber < number):
         print("Your number was too low than the number...")
+        trying += 1
     else:
         print()
 
-    resp = input("Do you wanna play again? (Y/exit)")
-    if(resp == "exit"):
-        playAgain == False
+    
